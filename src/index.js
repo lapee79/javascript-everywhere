8910281bf0a41d6f5c3c7c63b133dbdafc8ca834
Cheolgu Kim <cgkim@nowcom.com>
2022-03-31 09:06:54 -0700

    4.3

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-// index.js
-// This is the main entry point of our application
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+let notes = [
+    {id: '', content: '', author: ''},
+    {id: '', content: '', author: ''},
+    {id: '', content: '', author: ''}
+];
+
+// GraphQL schema language로 schema를 구성
+const typeDefs = gql`
+    type Query{
+        hello: String
+    }
+`;
+
+// schema field를 위한 resolver function 제공
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+};
+
+// Apollo server 설정
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apollo GraphQL middleware를 적용하고 경로를 /api로 설정
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({port}, () => 
+    console.log(
+        `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
+);
