ed0a1bb6db7590bf2fa9bde7bd4db70cff59b30f
Cheolgu Kim <cgkim@nowcom.com>
2022-04-11 00:48:45 -0700

    page 91

--- src/index.js
+++ src/index.js
@@ -1,52 +1,16 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
 
+// import local modules
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
+// .env file에 명시된 port 또는 port 4000에서 server를 실행
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-// GraphQL schema language로 schema를 구성
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-
-    type Query {
-        hello: String!
-        notes: [Note]!
-        note(id: ID!): Note!
-    }
-
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`;
-
-// schema field를 위한 resolver function 제공
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-        notes: async () => {
-            return await models.Note.find();
-        },
-        note: async (parent, args) => {
-            return await models.Note.findById(args.id);
-        }
-    },
-    Mutation: {
-        newNote: async (parent, args) => {
-            return await models.Note.create({
-                content: args.content,
-                author: 'Adam Scott'
-            });
-        }
-    }
-};
 
 const app = express();
 
@@ -54,7 +18,14 @@ const app = express();
 db.connect(DB_HOST);
 
 // Apollo server 설정
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => {
+        // context에 db models 추가
+        return { models };
+    }
+});
 
 // Apollo GraphQL middleware를 적용하고 경로를 /api로 설정
 server.applyMiddleware({ app, path: '/api' });
